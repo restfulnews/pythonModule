88bdc133aa5cf547bbebf04d80d6c8d1e7aecd4b
Oliver <oliver.dolk@live.com.au>
2018-04-17 12:07:08 +1000

    Update so that users can retrieve their token

--- pythonRestfulNews/__init__.py
+++ pythonRestfulNews/__init__.py
@@ -31,11 +31,20 @@ def search_news(token, topics, companyids, start_date, end_date):
         start_date=start_date,
         end_date=end_date
     )
-    print(headers)
     json_params = json.dumps(params)
     
     resp = requests.get(url=url, params=json_params, headers=headers)
-    print(resp)
     data = resp.json()
     
     return data
+
+def retrieve_token(email, password):
+    url = 'http://api.restfulnews.com/auth'
+    headers = {'Content-Type': 'application/json'}
+    data = dict(
+        email = email,
+        password = password
+    )
+    payload = json.dumps(data)
+    r = requests.post(url, headers=headers, data=payload)
+    return r.json()
